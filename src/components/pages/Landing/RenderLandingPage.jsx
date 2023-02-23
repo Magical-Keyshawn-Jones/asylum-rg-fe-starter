4ce24f95f85a88fbc0abd06bf851546afd0da030
Keyshawn Jones <keyshawnjones29@gmail.com>
2023-02-23 12:04:26 -0600

    finished deliverables

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,29 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div className="grant-rate-office-img">
+          <img src={GrantRatesByOfficeImg} alt="Search Grant Rates By Office" />
+          <p>Search Grant Rates By Office</p>
+        </div>
+
+        <div className="grant-rate-nationality-img">
+          <img
+            src={GrantRatesByNationalityImg}
+            alt="Search Grant Rates By Nationality"
+          />
+          <p>Search Grant Rates By Nationality</p>
+        </div>
+
+        <div className="grant-rate-over-time-img">
+          <img
+            src={GrantRatesOverTimeImg}
+            alt="Search Grant Rates By Over Time"
+          />
+          <p>Search Grant Rates By Over Time</p>
+        </div>
+      </div>
+
       <div className="view-more-data-btn-container">
         <Button
           type="default"
@@ -41,6 +63,15 @@ function RenderLandingPage(props) {
         >
           View the Data
         </Button>
+
+        <a href="https://humanrightsfirst.org/wp-content/uploads/2022/10/COW2021001887-I589Data.csv">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          >
+            Download Data
+          </Button>
+        </a>
       </div>
 
       <div className="middle-section">
@@ -61,7 +92,45 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className="bottom-section-container">
+            <div>
+              <h2>36%</h2>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020.
+              </p>
+            </div>
+
+            <div>
+              <h2>5%</h2>
+              <p>
+                The New York asylum office grant rate dropped to 5 percent in
+                fiscal year 2020.
+              </p>
+            </div>
+
+            <div>
+              <h2>6x Lower</h2>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office’s
+                average grant rate was six times lower than the San Francisco
+                asylum office.
+              </p>
+            </div>
+          </div>
+
+          <a
+            href="https://humanrightsfirst.org/library/uscis-records-reveal-systemic-disparities-in-asylum-decisions/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button>Read More</button>
+          </a>
+        </div>
+
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
